d24bc7c13223d843551481b7b380a7b6988768d8
fady <gamilfady605@gmail.com>
2022-03-16 00:41:21 +0200

    initial setup for first deployment

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+require("dotenv").config();
+require("colors");
+
+// initialize express server
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+	const info = {
+		"server is up": `server is up on port ${PORT}`,
+	};
+	console.table(info);
+});
