8496ed8a92a9089f6669b7aa20aac071a626c28a
fady <gamilfady605@gmail.com>
2022-03-16 01:49:14 +0200

    first deployment

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ require("colors");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+app.get("/", (req, res, next) => {
+	res.send("hello");
+});
 app.listen(PORT, () => {
 	const info = {
 		"server is up": `server is up on port ${PORT}`,
